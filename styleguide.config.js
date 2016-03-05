30c37580f3fdced7080ca4366cb807c9f04ab7d5
Artem Sapegin <artem@sapegin.ru>
2016-03-05 17:13:33 +0100

    Update react-styleguidist.

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,7 +1,26 @@
+const path = require("path")
 module.exports = {
   title: "PostCSS.org Style Guide",
-  rootDir: "./web_modules/",
-  components: "./**/*.js",
+  components: "./web_modules/**/*.js",
   serverPort: 3001,
   skipComponentsWithoutExample: true,
+  updateWebpackConfig: function(webpackConfig) {
+    const dir = path.join(__dirname, "web_modules")
+    webpackConfig.module.loaders.push(
+      {
+        test: /\.js$/,
+        include: dir,
+        loader: "babel",
+      },
+      {
+        test: /\.css$/,
+        include: dir,
+        loader: "style!css?modules&importLoaders=1!postcss-loader",
+      }
+    )
+    webpackConfig.postcss = [
+      require("postcss-cssnext")(),
+    ]
+    return webpackConfig
+  },
 }
