089489655b09b9f60bb1ff3aa58b5ee71577a9ee
Andrey Sitnik <andrey@sitnik.ru>
2020-06-21 18:45:22 -0400

    Move base styles

--- web_modules/layouts/Page/index.js
+++ web_modules/layouts/Page/index.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react"
 import { PropTypes } from "react"
-import Helmet from "react-helmet"
-import invariant from "invariant"
 
 // function pageDescription(text) {
 //   return text
@@ -22,34 +20,12 @@ export default class Page extends Component {
 
   render() {
     const {
-      pkg,
-    } = this.context.metadata
-
-    const {
       head,
       body,
     } = this.props
 
-    invariant(typeof head.title === "string", "Your page needs a title")
-
-    const meta = [
-      { property: "og:title", content: head.title },
-      { property: "og:type", content: "article" },
-      { property: "og:url", content: this.props.__url },
-      { property: "og:description", content: head.description },
-
-      { name: "twitter:card", content: "summary" },
-      { name: "twitter:title", content: head.title },
-      { name: "twitter:creator", content: `@${ pkg.twitter }` },
-      { name: "description", content: head.description },
-    ]
-
     return (
       <div>
-        <Helmet
-          title={ head.title }
-          meta={ meta }
-        />
 
         <h1>{ head.title }</h1>
         {
