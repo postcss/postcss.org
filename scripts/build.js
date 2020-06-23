4831705db58f148cdf3c527d8dee740e21677872
Andrey Sitnik <andrey@sitnik.ru>
2020-06-22 22:52:35 -0400

    Add production build optimizations

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
-let { join } = require('path')
+let { writeFile, readFile, unlink } = require('fs').promises
+let { join, extname } = require('path')
+let combineMedia = require('postcss-combine-media-query')
+let { terser } = require('rollup-plugin-terser')
+let { rollup } = require('rollup')
+let posthtml = require('posthtml')
+let postcss = require('postcss')
 let Bundler = require('parcel-bundler')
 let del = require('del')
 
@@ -11,10 +17,59 @@ async function cleanBuildDir () {
   await del(join(DIST, '*'), { dot: true })
 }
 
+function findAssets (bundle) {
+  return Array.from(bundle.childBundles).reduce(
+    (all, i) => {
+      return all.concat(findAssets(i))
+    },
+    [bundle.name]
+  )
+}
+
 async function build () {
   let bundler = new Bundler(join(SRC, 'index.pug'), { sourceMaps: false })
   await cleanBuildDir()
-  await bundler.bundle()
+  let [bundle, jsBundle] = await Promise.all([
+    bundler.bundle(),
+    rollup({ input: join(SRC, 'index.js'), plugins: [terser()] })
+  ])
+
+  let assets = findAssets(bundle)
+  let htmlFile = join(DIST, 'index.html')
+  let cssFile = assets.find(i => extname(i) === '.css')
+  let jsFile = assets.find(i => extname(i) === '.js')
+
+  let [html, css, jsOut] = await Promise.all([
+    readFile(htmlFile),
+    readFile(cssFile),
+    jsBundle.generate({ format: 'iife', strict: false }),
+    unlink(jsFile)
+  ])
+
+  let cssMin = postcss([combineMedia]).process(css, { from: cssFile }).css
+  let jsRepack = jsOut.output[0].code.trim()
+
+  function htmlPlugin (tree) {
+    tree.match({ tag: 'link', attrs: { rel: 'stylesheet' } }, () => {
+      return { tag: 'style', content: cssMin }
+    })
+    tree.match({ tag: 'script' }, i => {
+      if (i.attrs?.src?.includes('/src.')) {
+        return {
+          tag: 'script',
+          content: jsRepack
+        }
+      } else {
+        return i
+      }
+    })
+  }
+
+  let inlined = posthtml()
+    .use(htmlPlugin)
+    .process(html, { sync: true }).html
+
+  await Promise.all([writeFile(htmlFile, inlined), unlink(cssFile)])
 }
 
 build().catch(e => {
